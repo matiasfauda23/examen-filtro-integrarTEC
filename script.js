19bdcfa02b0b63f523bcd531f51fe96668e7f896
Matias Fauda <matiasfauda@gmail.com>
2026-04-23 19:33:10 -0300

    feat: create script.js and declare character and auxiliary variables

---
+++ script.js
@@ -0,0 +1,13 @@
+// Variables del personaje (
+const name = "Zara Darkbane"; 
+const characterClass = "Hechicera"; 
+let level = 7; 
+let health = 80; 
+let mana = 120; 
+const attack = 45; 
+const defense = 30; 
+
+// Variables auxiliares 
+const enemyDefense = 20; 
+const spellCost = 50; 
+const isStunned = false; 
